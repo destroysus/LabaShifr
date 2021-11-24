5ea79607546a7df3885993f9d252225c7fd9e116
destroysus <90268610+destroysus@users.noreply.github.com>
2021-11-24 18:54:16 +0500

    Добавил пояснения

--- Shifr.py
+++ Shifr.py
@@ -2,15 +2,17 @@ alfavit_EU =  'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 alfavit_RU = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 a='RU'
 b='EU'
+#Проверка правильности написания 
 while True:
     try:
         smeshenie = int(input('Введите число, равное смещению : '))
-        if smeshenie == int(smeshenie):
+        if smeshenie == int(smeshenie) and smeshenie >= 0:
             break
     except :
         print("Ошибка - это не число")
 message = input("Введите сообщение: ").upper()
 itog = ''
+#Проверка правильности написания 
 while True:
     try:
         language = input('Выберите язык RU/EU: ').upper()   
@@ -18,6 +20,7 @@ while True:
             break
     except :
         print("Введите RU(Русский язык) или EU(Английский язык)")
+#Блок высчитывания шифра Цезаря для Русского алфавита
 if language == 'RU':
     for i in message:
         mesto = alfavit_RU.find(i)
@@ -26,7 +29,8 @@ if language == 'RU':
             itog += alfavit_RU[new_mesto]
         else:
             itog += i
-else:
+#Блок высчитывания шифра Цезаря для Русского алфавита
+else: 
     for i in message:
         mesto = alfavit_EU.find(i)
         new_mesto = mesto + smeshenie
